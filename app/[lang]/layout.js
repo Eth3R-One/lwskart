8489210a0d8b996bde91fb4e001f0b5d6ac68d1d
Eth3R-One <refaythii@gmail.com>
2024-05-19 14:11:19 +0600

    add: Internationalization

---
+++ app/[lang]/layout.js
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+import "../globals.css";
+import "@ionic/react/css/core.css";
+
+import Header from "@/components/landing/Header";
+import NavBar from "@/components/landing/NavBar";
+import Footer from "@/components/landing/Footer";
+import CopyRightSection from "@/components/landing/CopyRightSection";
+import BreadCrumb from "@/components/landing/BreadCrumb";
+import { getDictionary } from "./dictionaries";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Home | LWSkart",
+  description:
+    "An ecommerce app created with nextjs with mongodb database and next-auth",
+};
+
+export default async function RootLayout({ params: { lang }, children }) {
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <NavBar dictionary={dictionary} />
+        <BreadCrumb />
+        {children}
+        <Footer />
+        <CopyRightSection />
+      </body>
+    </html>
+  );
+}
