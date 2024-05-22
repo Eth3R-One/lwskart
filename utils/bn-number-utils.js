4d3349266b6880c1e10939356e75c62bf3442579
Eth3R-One <refaythii@gmail.com>
2024-05-22 20:08:58 +0600

    fix: undefined value

--- utils/bn-number-utils.js
+++ utils/bn-number-utils.js
@@ -1,16 +1,18 @@
 const numbersInBn = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"];
 
 const convertNumberToBN = (number) => {
-  number = number.toString();
-  const numberInBn = number
-    ?.split("")
-    ?.map((num) => {
-      if (parseInt(num) >= 0 && parseInt(num) <= 9)
-        return numbersInBn[parseInt(num)];
-      else return num;
-    })
-    ?.join("");
-  return numberInBn;
+  if (number) {
+    number = number.toString();
+    const numberInBn = number
+      ?.split("")
+      ?.map((num) => {
+        if (parseInt(num) >= 0 && parseInt(num) <= 9)
+          return numbersInBn[parseInt(num)];
+        else return num;
+      })
+      ?.join("");
+    return numberInBn;
+  }
 };
 
 export default convertNumberToBN;
