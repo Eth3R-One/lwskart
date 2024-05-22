21a47acd118e7e1fa33923a986549587da783af0
Eth3R-One <refaythii@gmail.com>
2024-05-22 14:22:08 +0600

    add: product list

--- components/landing/products/ProductsList.jsx
+++ components/landing/products/ProductsList.jsx
@@ -1,10 +1,12 @@
 import ProductCard from "./ProductCard";
 
-const ProductsList = () => {
+const ProductsList = ({ products, lang }) => {
   return (
     <div className="col-span-3">
       <div className="grid md:grid-cols-3 grid-cols-2 gap-6">
-        <ProductCard />
+        {products?.map((product) => (
+          <ProductCard key={product.id} lang={lang} product={product} />
+        ))}
       </div>
     </div>
   );
