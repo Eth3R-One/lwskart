3a2856ebed019773ae654ae472972fb791fa7b7e
Eth3R-One <refaythii@gmail.com>
2024-05-22 14:20:50 +0600

    add: prouduct card

--- components/landing/products/ProductCard.jsx
+++ components/landing/products/ProductCard.jsx
@@ -1,63 +1,87 @@
-import CustomLink from "@/components/CustomLink";
 import Image from "next/image";
 
-const ProductCard = ({ product }) => {
+import CustomLink from "@/components/CustomLink";
+
+import convertNumberToBN from "@/utils/bn-number-utils";
+import { getDiscountPrice } from "@/utils/discount-price-utils";
+
+import { FaStar } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
+
+const ProductCard = ({ product, lang }) => {
   return (
     <div className="bg-white shadow rounded overflow-hidden group">
       <div className="relative">
         <Image
-          height={720}
+          height={1080}
           width={720}
-          src="/assets/images/products/product1.jpg"
-          alt="product 1"
+          src={product?.thumbnail}
+          alt={product?.title}
           className="w-full"
         />
         <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+          {/* view/add to cart */}
           <CustomLink
-            href="/shop/1"
+            href={`/shop/${product?.id}`}
             className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
             title="view product"
           >
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <FaMagnifyingGlass />
           </CustomLink>
           <a
             href="#"
             className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
             title="add to wishlist"
           >
-            <i className="fa-solid fa-heart"></i>
+            <FaHeart />
           </a>
         </div>
       </div>
       <div className="pt-4 pb-3 px-4">
-        <CustomLink href="/shop/1">
+        <CustomLink href={`shop/${product?.id}/`}>
           <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
-            Guyer Chair
+            {product?.title}
           </h4>
         </CustomLink>
         <div className="flex items-baseline mb-1 space-x-2">
-          <p className="text-xl text-primary font-semibold">$45.00</p>
-          <p className="text-sm text-gray-400 line-through">$55.90</p>
+          <p className="text-xl text-primary font-semibold">
+            $
+            {lang == "en"
+              ? Math.round(
+                  getDiscountPrice(product?.price, product?.discountPercentage)
+                )
+              : convertNumberToBN(
+                  Math.round(
+                    getDiscountPrice(
+                      product?.price,
+                      product?.discountPercentage
+                    )
+                  )
+                )}
+          </p>
+          {product?.discountPercentage && (
+            <p className="text-sm text-gray-400 line-through">
+              $
+              {lang == "en"
+                ? Math.round(product?.price)
+                : convertNumberToBN(Math.round(product?.price))}
+            </p>
+          )}
         </div>
         <div className="flex items-center">
           <div className="flex gap-1 text-sm text-yellow-400">
             <span>
-              <i className="fa-solid fa-star"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-star"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-star"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-star"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-star"></i>
+              <FaStar />
             </span>
           </div>
-          <div className="text-xs text-gray-500 ml-3">(150)</div>
+          <div className="text-xs text-gray-500 ml-3">
+            (
+            {lang == "en"
+              ? `${product?.feedBack?.reviews}`
+              : convertNumberToBN(product?.feedBack?.reviews)}
+            )
+          </div>
         </div>
       </div>
       <a
