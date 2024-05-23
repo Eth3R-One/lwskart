fe03b3c371907014f7622f25f8450f7851f82363
Eth3R-One <refaythii@gmail.com>
2024-05-24 00:54:09 +0600

    update: added searchParams feature

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -2,10 +2,22 @@
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-const CustomLink = ({ children, href, className, title }) => {
+const CustomLink = ({ children, href, searchParams, className, title }) => {
   const { lang } = useParams();
+  let link = `/${lang}/${href}`;
+  if (searchParams) {
+    const url = new URLSearchParams(); // Create a new URLSearchParams object
+
+    // Loop through the searchParams object and append each parameter
+    for (const key in searchParams) {
+      url.append(key, searchParams[key]);
+    }
+
+    // Append the search parameters to the link
+    link += `?${url.toString()}`;
+  }
   return (
-    <Link href={`/${lang}/${href}/`} className={className} title={title}>
+    <Link href={link} className={className} title={title}>
       {children}
     </Link>
   );
