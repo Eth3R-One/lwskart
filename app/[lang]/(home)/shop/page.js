9353280a56fbde0abe7918e6549599c89400edad
Eth3R-One <refaythii@gmail.com>
2024-05-23 23:46:13 +0600

    add: category filter

--- app/[lang]/(home)/shop/page.js
+++ app/[lang]/(home)/shop/page.js
@@ -4,13 +4,24 @@ import SideBar from "@/components/landing/products/SideBar";
 import { getProducts } from "@/database/queries";
 import Link from "next/link";
 
-const ShopPage = async ({ params: { lang } }) => {
+const refineSelectedCategory = (category) => {
+  const decoded = decodeURI(category);
+  if (decoded == "undefined") {
+    return "";
+  } else return decoded;
+};
+
+const ShopPage = async ({ params: { lang }, searchParams: { category } }) => {
   const products = await getProducts();
   return (
     <>
       <div className="container grid md:grid-cols-4 grid-cols-2 gap-6 pt-4 pb-16 items-start">
-        <SideBar />
-        <ProductsList products={products} lang={lang} />
+        <SideBar lang={lang} />
+        <ProductsList
+          products={products}
+          lang={lang}
+          selectedCategory={refineSelectedCategory(category)}
+        />
       </div>
     </>
   );
