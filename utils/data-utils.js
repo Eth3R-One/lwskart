1d368eda7e7f5359bcb666a94676dbe31650c2e2
Eth3R-One <refaythii@gmail.com>
2024-05-30 18:19:50 +0600

    fix

--- utils/data-utils.js
+++ utils/data-utils.js
@@ -1,12 +1,12 @@
 export const replaceMongoIdInArray = (array) => {
   const mappedArray = array
-    .map((item) => {
+    ?.map((item) => {
       return {
         id: item._id.toString(),
         ...item,
       };
     })
-    .map(({ _id, ...rest }) => rest);
+    ?.map(({ _id, ...rest }) => rest);
 
   return mappedArray;
 };
