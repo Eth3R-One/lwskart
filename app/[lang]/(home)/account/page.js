662c139f6c647cae2f4b41cdbf72bbfd979145b3
Eth3R-One <refaythii@gmail.com>
2024-06-01 13:39:40 +0600

    update: profile details

--- app/[lang]/(home)/account/page.js
+++ app/[lang]/(home)/account/page.js
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import PersonalProfileForm from "@/components/auth/profile/PersonalProfileForm";
 import ShippingAddressForm from "@/components/auth/profile/ShippingAddressForm";
 import BillingAddressForm from "@/components/auth/profile/BillingAddressForm";
+import { getUserAddress, getUserById } from "@/database/queries";
 
 const AccountPage = async ({ params: { lang } }) => {
   const dictionary = await getDictionary(lang);
@@ -11,15 +12,27 @@ const AccountPage = async ({ params: { lang } }) => {
   if (!session?.user) {
     redirect(`/${lang}/login`);
   }
+
+  const user = await getUserById(session?.user?.id);
+
+  const address = await getUserAddress(session?.user?.id);
   return (
     <>
       <div className="container  items-start gap-6 pt-4 pb-16">
         <div className=" grid grid-cols-3 gap-4 mx-auto max-w-5xl">
-          <PersonalProfileForm dictionary={dictionary} />
+          <PersonalProfileForm dictionary={dictionary} user={user} />
 
-          <ShippingAddressForm dictionary={dictionary} />
+          <ShippingAddressForm
+            dictionary={dictionary}
+            user={user}
+            address={address}
+          />
 
-          <BillingAddressForm dictionary={dictionary} />
+          <BillingAddressForm
+            dictionary={dictionary}
+            user={user}
+            address={address}
+          />
         </div>
       </div>
     </>
