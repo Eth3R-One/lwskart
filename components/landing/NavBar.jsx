e017dcedccf076fe7679ccc9c9cb8ac07d1c00d4
Eth3R-One <refaythii@gmail.com>
2024-06-04 18:14:12 +0600

    fix ui

--- components/landing/NavBar.jsx
+++ components/landing/NavBar.jsx
@@ -4,10 +4,15 @@ import CustomLink from "@/components/CustomLink";
 import { auth } from "@/auth";
 import Image from "next/image";
 import Logout from "../auth/Logout";
+import { getCategories } from "@/database/queries";
 
 const NavBar = async ({ lang, dictionary, showLogin }) => {
   const Icon = IoIosLogIn;
   const session = await auth();
+
+  const allCategories = await getCategories();
+  console.log(allCategories);
+
   return (
     <nav className="bg-gray-800">
       <div className="container flex">
@@ -23,72 +28,23 @@ const NavBar = async ({ lang, dictionary, showLogin }) => {
             className="absolute left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible w-[600px]"
             style={{ width: "300px" }}
           >
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/sofa.svg"
-                alt="sofa"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Sofa</span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/terrace.svg"
-                alt="terrace"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Living Room</span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/bed.svg"
-                alt="bed"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Bedroom</span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/office.svg"
-                alt="Outdoor"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Outdoor</span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/outdoor-cafe.svg"
-                alt="outdoor"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Outdoor</span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-            >
-              <img
-                src="/assets/images/icons/bed-2.svg"
-                alt="Mattress"
-                className="w-5 h-5 object-contain"
-              />
-              <span className="ml-6 text-gray-600 text-sm">Mattress</span>
-            </a>
+            {allCategories?.map((cat) => (
+              <CustomLink
+                key={cat?.id}
+                href={`shop`}
+                searchParams={{ category: cat?.name }}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <Image
+                  src={cat?.thumbnail}
+                  alt={cat.name}
+                  height={24}
+                  width={24}
+                  className="w-5 h-5 object-contain"
+                />
+                <span className="ml-6 text-gray-600 text-sm">{cat?.name}</span>
+              </CustomLink>
+            ))}
           </div>
         </div>
 
