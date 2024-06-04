90084b7fe40a240d7db96cd4cf72dc52ebd7aa52
Eth3R-One <refaythii@gmail.com>
2024-06-04 14:40:23 +0600

    add: details

--- app/[lang]/(home)/account/page.js
+++ app/[lang]/(home)/account/page.js
@@ -4,7 +4,13 @@ import { redirect } from "next/navigation";
 import PersonalProfileForm from "@/components/auth/profile/PersonalProfileForm";
 import ShippingAddressForm from "@/components/auth/profile/ShippingAddressForm";
 import BillingAddressForm from "@/components/auth/profile/BillingAddressForm";
-import { getUserAddress, getUserById } from "@/database/queries";
+import {
+  getOrderHistory,
+  getUserAddress,
+  getUserById,
+} from "@/database/queries";
+import Link from "next/link";
+import CustomLink from "@/components/CustomLink";
 
 const AccountPage = async ({ params: { lang } }) => {
   const dictionary = await getDictionary(lang);
@@ -16,6 +22,18 @@ const AccountPage = async ({ params: { lang } }) => {
   const user = await getUserById(session?.user?.id);
 
   const address = await getUserAddress(session?.user?.id);
+
+  const orders = await getOrderHistory(session?.user?.id);
+
+  const formatDate = (orderDate) => {
+    const date = new Date(orderDate);
+    return `${date.getDate().toString().padStart(2, "0")}-${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}-${date.getFullYear()}`;
+  };
+
   return (
     <>
       <div className="container  items-start gap-6 pt-4 pb-16">
@@ -33,6 +51,90 @@ const AccountPage = async ({ params: { lang } }) => {
             user={user}
             address={address}
           />
+        </div>
+        <div className="pt-10">
+          <div>
+            <p className="text-center text-3xl">Order History</p>
+            <div>
+              {orders.length > 0 ? (
+                <div className="p-2">
+                  <div>
+                    <div className="px-5">
+                      {orders?.map((order) => (
+                        <div key={order?.id} className="container mb-10">
+                          <div className="col-span-9 mt-9">
+                            <div>
+                              <div className="border-separate border border-gray-200 p-5 shadow-lg rounded-md">
+                                <div className="flex justify-between items-center text-center">
+                                  <div>
+                                    <p className="my-2 font-medium">
+                                      Order Number
+                                    </p>
+                                    <p className="text-sm text-gray-500">
+                                      {order?.id}
+                                    </p>
+                                  </div>
+                                  <div>
+                                    <p className="my-2 font-medium">
+                                      Purchased
+                                    </p>
+                                    <p>{formatDate(order.createdAt)}</p>
+                                  </div>
+                                  <div>
+                                    <p className="my-2 font-medium">Items</p>
+                                    <p>{order?.items?.length}</p>
+                                  </div>
+                                  <div>
+                                    <p className="my-2 font-medium">Total</p>
+                                    <p>${order?.totalAmount}</p>
+                                  </div>
+                                  <div className="mr-14">
+                                    <p className="my-2 font-medium">Status</p>
+                                    <p className="text-red-400">
+                                      {order?.status}
+                                    </p>
+                                  </div>
+
+                                  <div className="mr-14">
+                                    <p className="my-2 font-medium">Payment</p>
+                                    <p className=" px-1 rounded-md">
+                                      {order?.paymentMethod}
+                                    </p>
+                                    {order?.paymentStatus == "Paid" ? (
+                                      <p className="bg-green-500 text-white rounded-md">
+                                        {order?.paymentStatus}
+                                      </p>
+                                    ) : (
+                                      <p className="bg-red-500 text-white rounded-md">
+                                        {order?.paymentStatus}
+                                      </p>
+                                    )}
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        /* </div> */
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="p-10 items-center justify-center text-center gap-2">
+                    <p className="">You have no order</p>
+                    <CustomLink
+                      href={`${lang}/shop/`}
+                      className={"bg-purple-400 rounded-md px-2"}
+                    >
+                      Go to Shop
+                    </CustomLink>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
